26d1352b7a89ca518cb2abe52d0ca0a950e4c30d
Jan Neumann <66623604+janeumnn@users.noreply.github.com>
2025-04-03 00:04:34 +0200

    chore: update to tailwindcss@4.1.1 and daisyui@5.0.12
    includes also update of related packages

--- apps/web/astro.config.js
+++ apps/web/astro.config.js
@@ -1,10 +1,11 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: 1337, host: true },
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
--- apps/web/prettier.config.js
+++ apps/web/prettier.config.js
@@ -4,5 +4,6 @@ import baseConfig from '@janeumnn/prettier-config';
 export default {
   ...baseConfig,
   plugins: baseConfig.plugins?.concat(['prettier-plugin-astro', 'prettier-plugin-tailwindcss']),
+  tailwindStylesheet: './src/styles/index.css',
   overrides: [{ files: '*.astro', options: { parser: 'astro' } }]
 };
